6088deb428fdd69b50d719da5f6cabebb834c200
Iago <iago@debigv.home>
2023-02-23 11:30:52 +0100

    init

---
+++ preview.r
@@ -0,0 +1,38 @@
+# preview.r
+
+
+# OS dependencies ----------------------------------------------------------
+
+
+if(Sys.info()["sysname"] == "Linux"){
+	data_path <- "~/Dropbox/juanpablo spanish pain study/"
+	dev_lib_path <- "~/R/x86_64-pc-linux-gnu-library/dev"
+} else if(Sys.info()["sysname"] == "Windows"){
+	data_path <- paste0(grep("^[A-Z]:$", sub(":(.*)", ":",shell("wmic logicaldisk get name", intern = TRUE)), value = TRUE), "/PSSJD/PADRIS-COVID/data")
+	data_path <- data_path[file.exists(data_path)]
+	data_path <- paste0(data_path, "/")
+	dev_lib_path <- .libPaths()
+}
+
+
+
+# Libraries ---------------------------------------------------------------
+
+library(haven)
+library(gimme)
+
+
+
+
+# Data loading --------------------------------------------------------------
+
+impactdt <- read_sav(paste0(data_path, "Dataset_IMPACT-EMA_v2.sav"))
+
+
+
+
+# Data arranging and merging ----------------------------------------------
+
+
+impactdt <- as_factor(impactdt)
+
