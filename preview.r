cebcff72e4e8ddd2b053d3f9dc1e7cec06d921b7
iago-pssjd <iago.gine@sjd.es>
2023-03-21 08:23:36 +0100

    update address

--- preview.r
+++ preview.r
@@ -8,7 +8,7 @@ if(Sys.info()["sysname"] == "Linux"){
 	data_path <- "~/Dropbox/juanpablo spanish pain study/"
 	dev_lib_path <- "~/R/x86_64-pc-linux-gnu-library/dev"
 } else if(Sys.info()["sysname"] == "Windows"){
-	data_path <- paste0(grep("^[A-Z]:$", sub(":(.*)", ":",shell("wmic logicaldisk get name", intern = TRUE)), value = TRUE), "/PSSJD/IMPACT/data")
+	data_path <- paste0(grep("^[A-Z]:$", sub(":(.*)", ":",shell("wmic logicaldisk get name", intern = TRUE)), value = TRUE), "/NCext/PSSJD_other/IMPACT")
 	data_path <- data_path[file.exists(data_path)]
 	data_path <- paste0(data_path, "/")
 	dev_lib_path <- .libPaths()
