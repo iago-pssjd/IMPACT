45c776834ae97b57a62d7426d8712ac052a842b3
iago-pssjd <iago.gine@sjd.es>
2023-03-27 09:09:29 +0200

    vim formatting

--- analyses.r
+++ analyses.r
@@ -34,52 +34,51 @@ trellis.par.set("dot.symbol", dot.symbol)
 
 # Auxiliar functions ------------------------------------------------------
 
-dotplot.lmerMod <- function (x, data, main = TRUE, transf = I, ...) {
-  xf <- fixef(x)
-  x <- ranef(x, condVar = TRUE)
-  for(grf in names(x)){
-    for(erf in names(x[[grf]]))
-      x[[grf]][, erf] <- x[[grf]][, erf] + xf[erf]
-  }
-  dotplot(x, data = data, main = main, transf = I, v = xf, ...)
+dotplot.lmerMod <- function(x, data, main = TRUE, transf = I, ...){
+	xf <- fixef(x)
+	x <- ranef(x, condVar = TRUE)
+	for(grf in names(x)){
+		for(erf in names(x[[grf]]))
+			x[[grf]][, erf] <- x[[grf]][, erf] + xf[erf]
+	}
+	dotplot(x, data = data, main = main, transf = I, v = xf, ...)
 }
-  
 
-dotplot.ranef.mer <- function(x, data, main = TRUE, transf = I, ...) 
-{
-  prepanel.ci <- function(x, y, se, subscripts, ...) {
-    if (is.null(se)) return(list())
-    x <- as.numeric(x)
-    hw <- 1.96 * as.numeric(se[subscripts])
-    list(xlim = range(transf(x - hw), transf(x + hw), finite = TRUE))
-  }
-  panel.ci <- function(x, y, se, subscripts, pch = 16, horizontal = TRUE, v = 0,
-                       col = dot.symbol$col, cex = dot.symbol$cex, lty.h = dot.line$lty, lty.v = dot.line$lty, lwd.h = dot.line$lwd, lwd.v = dot.line$lwd, 
-                       col.line.h = dot.line$col, col.line.v = dot.line$col, levels.fos = unique(y), groups = NULL, 
-                       ...) {
-    x <- as.numeric(x)
-    y <- as.numeric(y)
-    dot.line <- trellis.par.get("dot.line")
-    dot.symbol <- trellis.par.get("dot.symbol")
-    sup.symbol <- trellis.par.get("superpose.symbol")
-    panel.abline(h = levels.fos, col = col.line.h, lty = lty.h, 
-                 lwd = lwd.h)
-    panel.abline(v = v[panel.number()], col = col.line.v, lty = lty.v, lwd = lwd.v)
-    if (!is.null(se)) {
-      se <- as.numeric(se[subscripts])
-      panel.segments(transf(x - 1.96 * se), y, transf(x + 
-                                                        1.96 * se), y, col = "black")
-    }
-    panel.xyplot(transf(x), y, pch = pch, col = col, cex = cex, ...)
-  }
-  f <- function(nx, ...) {
-    ss <- lme4:::asDf0(x, nx)
-    mtit <- if (main) 
-      nx
-    dotplot(.nn ~ values | ind, ss, se = ss$se, prepanel = prepanel.ci, 
-            panel = panel.ci, xlab = NULL, main = mtit, ...)
-  }
-  setNames(lapply(names(x), f, ...), names(x))
+
+dotplot.ranef.mer <- function(x, data, main = TRUE, transf = I, ...){
+	prepanel.ci <- function(x, y, se, subscripts, ...){
+		if (is.null(se)) return(list())
+		x <- as.numeric(x)
+		hw <- 1.96 * as.numeric(se[subscripts])
+		list(xlim = range(transf(x - hw), transf(x + hw), finite = TRUE))
+	}
+	panel.ci <- function(x, y, se, subscripts, pch = 16, horizontal = TRUE, v = 0,
+			     col = dot.symbol$col, cex = dot.symbol$cex, lty.h = dot.line$lty, lty.v = dot.line$lty, lwd.h = dot.line$lwd, lwd.v = dot.line$lwd, 
+			     col.line.h = dot.line$col, col.line.v = dot.line$col, levels.fos = unique(y), groups = NULL, 
+			     ...){
+		x <- as.numeric(x)
+		y <- as.numeric(y)
+		dot.line <- trellis.par.get("dot.line")
+		dot.symbol <- trellis.par.get("dot.symbol")
+		sup.symbol <- trellis.par.get("superpose.symbol")
+		panel.abline(h = levels.fos, col = col.line.h, lty = lty.h, 
+			     lwd = lwd.h)
+		panel.abline(v = v[panel.number()], col = col.line.v, lty = lty.v, lwd = lwd.v)
+		if (!is.null(se)) {
+			se <- as.numeric(se[subscripts])
+			panel.segments(transf(x - 1.96 * se), y, transf(x + 
+									1.96 * se), y, col = "black")
+		}
+		panel.xyplot(transf(x), y, pch = pch, col = col, cex = cex, ...)
+	}
+	f <- function(nx, ...){
+		ss <- lme4:::asDf0(x, nx)
+		mtit <- if (main) 
+			nx
+		dotplot(.nn ~ values | ind, ss, se = ss$se, prepanel = prepanel.ci, 
+			panel = panel.ci, xlab = NULL, main = mtit, ...)
+	}
+	setNames(lapply(names(x), f, ...), names(x))
 }
 
 
@@ -112,29 +111,29 @@ colnames(beta10) <- colnames(beta90) <- colnames(beta) <- colnames(chisq) <- col
 # Multilevel analyses -----------------------------------------------------
 
 for(ixout in seq_along(outcomes)){
-  for(ixproc in seq_along(processes)){
-    iout <- outcomes[ixout]
-    iproc <- processes[ixproc]
-    # rimodel <- lme(reformulate(iproc, response = iout), data = impactdtres, random = ~ 1 | ParticipantID, method = "ML", na.action = na.omit)
-    # rsmodel <- lme(reformulate(iproc, response = iout), data = impactdtres, random = as.formula(paste("~", iproc, "| ParticipantID")), method = "ML", na.action = na.omit)
-    # chisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["L.Ratio"]][2]
-    # pchisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["p-value"]][2]
-    # beta10[ixproc, ixout] <- quantile(coef(rsmodel)[[iproc]], probs = c(0.1))
-    # beta90[ixproc, ixout] <- quantile(coef(rsmodel)[[iproc]], probs = c(0.9))
-    # plot(coef(rsmodel), panel = function(...) {panel.dotplot(..., col = 'darkgreen'); panel.abline(v = fixef(rsmodel), lty = 3, col = 'purple')})
+	for(ixproc in seq_along(processes)){
+		iout <- outcomes[ixout]
+		iproc <- processes[ixproc]
+		# rimodel <- lme(reformulate(iproc, response = iout), data = impactdtres, random = ~ 1 | ParticipantID, method = "ML", na.action = na.omit)
+		# rsmodel <- lme(reformulate(iproc, response = iout), data = impactdtres, random = as.formula(paste("~", iproc, "| ParticipantID")), method = "ML", na.action = na.omit)
+		# chisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["L.Ratio"]][2]
+		# pchisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["p-value"]][2]
+		# beta10[ixproc, ixout] <- quantile(coef(rsmodel)[[iproc]], probs = c(0.1))
+		# beta90[ixproc, ixout] <- quantile(coef(rsmodel)[[iproc]], probs = c(0.9))
+		# plot(coef(rsmodel), panel = function(...) {panel.dotplot(..., col = 'darkgreen'); panel.abline(v = fixef(rsmodel), lty = 3, col = 'purple')})
 
-    rimodel <- lmer(paste0(iout, " ~ ", iproc, " + (1|ParticipantID)"), data = impactdtres, REML = FALSE)
-    rsmodel <- lmer(paste0(iout, " ~ ", iproc, " + (", iproc,"|ParticipantID)"), data = impactdtres, REML = FALSE)
-    # ranef(rsmodel)[["ParticipantID"]][,"(Intercept)"] # merMod methods
-    # ranef(rsmodel)[["ParticipantID"]][,iproc] # merMod methods
-    chisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["Chisq"]][2]
-    pchisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["Pr(>Chisq)"]][2]
-    beta[ixproc, ixout] <- fixef(rsmodel)[[iproc]]
-    beta10[ixproc, ixout] <- quantile(coef(rsmodel)[["ParticipantID"]][[iproc]], probs = c(0.1))
-    beta90[ixproc, ixout] <- quantile(coef(rsmodel)[["ParticipantID"]][[iproc]], probs = c(0.9))
-    # plot(compareFits(coef(rsmodel), coef(rimodel)), mark = fixef(rsmodel))
-    dotplot(rsmodel, col = 'darkgreen', col.line.v = 'purple', lty.v = 3, scales = list(x = list(relation = 'free')))[["ParticipantID"]]
-  }
+		rimodel <- lmer(paste0(iout, " ~ ", iproc, " + (1|ParticipantID)"), data = impactdtres, REML = FALSE)
+		rsmodel <- lmer(paste0(iout, " ~ ", iproc, " + (", iproc,"|ParticipantID)"), data = impactdtres, REML = FALSE)
+		# ranef(rsmodel)[["ParticipantID"]][,"(Intercept)"] # merMod methods
+		# ranef(rsmodel)[["ParticipantID"]][,iproc] # merMod methods
+		chisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["Chisq"]][2]
+		pchisq[ixproc, ixout] <- anova(rsmodel, rimodel)[["Pr(>Chisq)"]][2]
+		beta[ixproc, ixout] <- fixef(rsmodel)[[iproc]]
+		beta10[ixproc, ixout] <- quantile(coef(rsmodel)[["ParticipantID"]][[iproc]], probs = c(0.1))
+		beta90[ixproc, ixout] <- quantile(coef(rsmodel)[["ParticipantID"]][[iproc]], probs = c(0.9))
+		# plot(compareFits(coef(rsmodel), coef(rimodel)), mark = fixef(rsmodel))
+		dotplot(rsmodel, col = 'darkgreen', col.line.v = 'purple', lty.v = 3, scales = list(x = list(relation = 'free')))[["ParticipantID"]]
+	}
 }
 
 coef(rsmodel)
