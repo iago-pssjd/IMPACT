03e05d61e86684aaec417336d19250ba600b29cf
Iago Giné-Vázquez <iago@tutamail.com>
2023-09-16 01:49:33 +0200

    idplots without metaanalyses including previous code versions

--- analyses.r
+++ analyses.r
@@ -628,20 +628,29 @@ for(idx in seq_len(nrow(impactIDs))){
 		iout <- outcomes[ixout]
 		iout2 <- names(outcomes)[ixout]
 		idout <-iDout[ParticipantID == idsp & outcome == iout, .(ParticipantID, outcome, beta, SE, process, outN, procN)]
-		res.id <- try(rma(yi = beta, sei = SE, data = idout, measure = "GEN", method = "REML"), silent = TRUE)
-		if(!inherits(res.id, what = "try-error", which = FALSE)){
-			ci.lb <- with(idout, beta - SE * qnorm(0.05/2, lower.tail = FALSE))
-			ci.ub <- with(idout, beta + SE * qnorm(0.05/2, lower.tail = FALSE))
-			png(paste0(data_NCpath, "graphics/iARIMAX/idplots2/", iout, "-id", idsp, ".png"), bg = "transparent", width = 8.33, height = 6.61, units = "cm", pointsize = 5, res = 320, type = "cairo")
-			#svg(paste0(data_NCpath, "graphics/iARIMAX/idplots/", iout, "-id", idsp, ".svg"), bg = "transparent", width = 7, height = 7)
-			print(idforest <- forest(x = res.id, slab = idout$procN, main = NA))
-			par(new = TRUE)
-			forest(res.id$yi, res.id$vi, level = 95, lwd = 1, annotate = FALSE, psize = NA, refline = NA, slab = NA, xaxt = "n", xlab = "", lty = c("solid", "solid"), col = ifelse(na.omit(between(0, ci.lb, ci.ub, NAbounds = NA)), "red", "green"), xlim = idforest$xlim, ylim = idforest$ylim)
-			# print(idforest <- forest(x = idout$beta, sei = idout$SE, slab = rep("", nrow(idout)), col = ifelse(between(0, ci.lb, ci.ub), "red", "green"), annotate = FALSE, main = paste(iout, idsp)))
-			# print(text(idforest$textpos[1], seq(nrow(idout), 1, -1), idout$process, pos = 4))
-			dev.off()
-		}
-		rm(res.id)
+		# res.id <- try(rma(yi = beta, sei = SE, data = idout, measure = "GEN", method = "REML"), silent = TRUE)
+		idout <- na.omit(idout)
+		# if(!inherits(res.id, what = "try-error", which = FALSE)){
+		ci.lb <- with(idout, beta - SE * qnorm(0.05/2, lower.tail = FALSE))
+		ci.ub <- with(idout, beta + SE * qnorm(0.05/2, lower.tail = FALSE))
+		png(paste0(data_NCpath, "graphics/iARIMAX/idplots/", iout, "-id", idsp, ".png"), bg = "transparent", width = 8.33, height = 6.61, units = "cm", pointsize = 5, res = 320, type = "cairo")
+		#svg(paste0(data_NCpath, "graphics/iARIMAX/idplots/", iout, "-id", idsp, ".svg"), bg = "transparent", width = 7, height = 7)
+		# print(idforest <- forest(x = res.id, slab = idout$procN, main = NA))
+		# par(new = TRUE)
+		# forest(res.id$yi, res.id$vi, level = 95, lwd = 1, annotate = FALSE, psize = NA, refline = NA, slab = NA, xaxt = "n", xlab = "", lty = c("solid", "solid"), col = ifelse(na.omit(between(0, ci.lb, ci.ub, NAbounds = NA)), "red", "green"), xlim = idforest$xlim, ylim = idforest$ylim)
+		print(idforest <- forest(x = idout$beta, sei = idout$SE, slab = idout$procN, lty = c("blank", "blank"), col = "black", annotate = TRUE, main = NA))
+		# print(idforest <- forest(x = idout$beta, sei = idout$SE, slab = rep("", nrow(idout)), col = ifelse(between(0, ci.lb, ci.ub), "red", "green"), annotate = TRUE, main = NA))
+		# adding left text
+		# print(text(idforest$textpos[1], seq(nrow(idout), 1, -1), idout$procN, pos = 4))
+		# adding right black text
+		# par(new = TRUE)
+		# forest(x = idout$beta, sei = idout$SE, lwd = 1, annotate = TRUE, refline = NA, slab = NA, xaxt = "n", xlab = "", lty = c("blank", "blank"), col = "black", xlim = idforest$xlim, ylim = idforest$ylim)
+		# next: adding coloured CI lines on (in front of) estimate black point
+		par(new = TRUE)
+		forest(x = idout$beta, sei = idout$SE, lwd = 1, annotate = FALSE, psize = NA, refline = NA, slab = NA, xaxt = "n", xlab = "", lty = c("solid", "solid"), col = ifelse(between(0, ci.lb, ci.ub, NAbounds = NA), "red", "green"), xlim = idforest$xlim, ylim = idforest$ylim)
+		dev.off()
+		# }
+		# rm(res.id)
 	}
 }
 
